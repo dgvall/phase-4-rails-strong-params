684a2004b4098c87d9a4fa62aa21e986fca86923
Diego Valles <dgvall134@gmail.com>
2023-02-20 20:03:17 -0700

    commit tree 3d6b643c370a22d5dba5ec4c977a8c94fc651562

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,5 +1,5 @@
 class BirdsController < ApplicationController
-
+wrap_parameters format: []
   # GET /birds
   def index
     birds = Bird.all
@@ -7,10 +7,10 @@ class BirdsController < ApplicationController
   end
 
   # POST /birds
-  def create
-    bird = Bird.create(name: params[:name], species: params[:species])
-    render json: bird, status: :created
-  end
+ def create
+  bird = Bird.create(params.permit(:name, :species))
+  render json: bird, status: :created
+end
 
   # GET /birds/:id
   def show
